e86e56d5d4b930bbb4c5429aac66dd254d576e49
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-08 00:08:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,8 +26,15 @@ class App < Sinatra::Base
     @word4 = @params[:word4]
     @word5 = @params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
-   
   end 
+  
+  get '/:operation/:number1/:number2' do 
+    @operation = @params[:operation]
+    @number1 = @params[:number1]
+    @number2 = @params[:number2]
+    @number1.to_i @operation @number2.to_i 
+  end
+    
   
   
 end
