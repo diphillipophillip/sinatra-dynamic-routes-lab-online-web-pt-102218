8e1a30a8210266ef8acec611850fd0e3c1a063a7
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-08 00:15:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,12 @@ class App < Sinatra::Base
     @number2 = @params[:number2]
     if @operation == "add"
       @number1.to_i + @number2.to_i 
-      
+      elsif @operation == "subtract"
+      @number1.to_i - @number2.to_i 
+      elsif @operation == "multiply"
+      @number1.to_i * @number2.to_i 
+      elsif @operation == "divide"
+      @number1.to_i / @number2.to_i 
     end 
   end
     
